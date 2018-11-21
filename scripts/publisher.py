99ce20b5cc18122ba07b186cb64ea1ecf55f9827
Abdulhamid Abdo <abdoabdulhamid@gmail.com>
2018-11-21 12:44:53 +0300

    Added a total counter to sum results being published.

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from random import randint,choice
+import string
+def generateRandomResult():
+	result = randint(0,101)
+	return result
+
+def generateRandomletter():
+	return choice(string.ascii_uppercase) 
+
 
 def publisher():
-	pub = rospy.Publisher('NumberGenerator',String, queue_size=10)
+	pub = rospy.Publisher('NumberGenerator', String, queue_size=10)
 	rospy.init_node('publisher', anonymous=True)
-	rate = rospy.Rate(10)
+	rate = rospy.Rate(1)
 	while not rospy.is_shutdown():
-		hello_str = 'Hello World %s' % rospy.get_time()
-		rospy.loginfo(hello_str)
-		pub.publish(hello_str)
+		studeent_name = generateRandomletter()
+		result = generateRandomResult()
+		# hello_str = 'Hello World %s' % rospy.get_time()
+		# rospy.loginfo(hello_str)
+		# pub.publish(hello_str)
+		rospy.loginfo(result)
+		pub.publish(str(result))
 		rate.sleep()
 
 if __name__ == '__main__':
 	try:
 		publisher()
-	except rospy.ROSInturruptEException:
+	except rospy.ROSInterruptException:
 		pass
 
 
--- scripts/suscriber.py
+++ scripts/suscriber.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+total = 90
+
+def calculateTotal(result):
+	global total 
+	rospy.loginfo(total)
+	total = total + int(result)
+
+def printInfo():
+	print("Publisher is down")
+
 def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + "I heared %s", data.data)
+	calculateTotal(data.data)
+	rospy.loginfo(rospy.get_caller_id() + "I got %s", data.data)
 
 def subscriber():
+	total = 0
 	rospy.init_node('subscriber', anonymous=True)
-	rospy.Subscriber('NumberGenerator', String,callback)
+	sub = rospy.Subscriber('NumberGenerator', String,callback)
+	rospy.on_shutdown(printInfo)
+
+	# while sub.get_num_connections() == 0:
+	# 	print("heere",sub.get_num_connections(), rospy.is_shutdown())
+	# 	rospy.signal_shutdown ("publisher closed");
 	rospy.spin()
 
 if __name__ == '__main__':
